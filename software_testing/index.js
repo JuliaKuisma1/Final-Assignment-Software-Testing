85802592e1d08f493f848741bc5b5c049286309e
Julzerino <kuismajulia97@gmail.com>
2019-11-13 11:16:08 +0200

    Initialize of the project

---
+++ software_testing/index.js
@@ -0,0 +1,17 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const files = require('./lib/files');
+
+clear();
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('Software Testing', {horizontalLayout: 'full'})
+    )
+);
+
+if (files.directoryExists('.git')) {
+    console.log(chalk.red('Already a Git repository!'));
+    process.exit();
+}
